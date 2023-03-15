1e072acb2ad1b4d11fd7a00b0c628be85d5826a9
Ranonx <108208227+Ranonx@users.noreply.github.com>
2023-03-15 17:56:40 +0800

    express files

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(logger);
+app.use(express.static(path.join(__dirname, "public"))); // add this line to serve static files
 
 // mySQL connection
 const connection = mysql.createConnection({
