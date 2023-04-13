59efe7b4e67aafec009aeb0084234d78035122a3
Ranonx <108208227+Ranonx@users.noreply.github.com>
2023-04-13 15:38:39 +0800

    welcome msg update

--- route.js
+++ route.js
@@ -3,12 +3,6 @@ const { sendmess } = require("./sendmess");
 const express = require("express");
 const router = express.Router();
 const moment = require("moment-timezone");
-const {
-  bookAppointment,
-  getAppointmentDateById,
-  queryAppointment,
-  cancelAppointment,
-} = require("./dbQueries");
 
 // Handle subscription event
 router.post("/", async (req, res) => {
@@ -22,7 +16,13 @@ router.post("/", async (req, res) => {
         touser: FromUserName,
         msgtype: "text",
         text: {
-          content: "欢迎关注伊甸智的公众号！",
+          content: `欢迎关注伊甸数智医疗技术!
+
+          专注于数字化骨科康复和假肢产品的研发，旗下品牌专注于足和脊柱健康产品的研发。我们的目标是利用高新技术手段，研究数字化骨科康复生物力学和人工智能应用，帮助人们更好地恢复和保护身体健康。
+          
+          如果您是学校筛查报告的家长或学生，欢迎使用我们的在线足部扫描服务！我们将为您提供详细的报告和建议，帮助您更好地了解和保护自己的足部健康。请点击下方的[报告查询]，或者[我的报告]，以开始您的足部扫描之旅。
+          
+          感谢您的关注，我们期待与您共同探索数字化医疗技术的未来。`,
         },
       });
       console.log("发送消息成功", result);
@@ -34,97 +34,6 @@ router.post("/", async (req, res) => {
   } else {
     res.send("success");
   }
-});
-
-router.get("/login", async (req, res) => {
-  res.sendFile(path.join(__dirname, "login.html"));
-});
-
-// 预约页面 push
-router.post("/booking", (req, res) => {
-  const { name, phone, appointment_date } = req.body;
-  const openid = req.headers["x-wx-openid"];
-  console.log(
-    `Received booking request with name: ${name}, phone: ${phone}, openid: ${openid}, and date:${appointment_date}`
-  );
-
-  const values = [name, phone, appointment_date, openid];
-
-  bookAppointment(values, (error, results) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send("Failed to book appointment.");
-    } else {
-      const id = results.insertId;
-
-      getAppointmentDateById(id, (error, results) => {
-        if (error) {
-          console.error(error);
-          res.status(500).send("Failed to retrieve appointment date.");
-        } else {
-          console.log(results);
-          const appointment_date = moment
-            .tz(results[0].appointment_date, "Asia/Shanghai")
-            .format("YYYY-MM-DD HH:mm");
-          res.header("Content-Type", "application/json");
-          res.send({
-            message: "Appointment booked successfully.",
-            appointment_date,
-          });
-        }
-      });
-    }
-  });
-});
-
-// 查询预约时间
-router.post("/query", (req, res) => {
-  const { name, phone } = req.body;
-  console.log(`Received query request with name: ${name} and phone: ${phone}`);
-
-  queryAppointment(name, phone, (error, results) => {
-    console.log(results);
-    if (error) {
-      console.error(error);
-      res
-        .status(500)
-        .json({ message: "An error occurred while querying the database." });
-    } else if (results.length === 0) {
-      res.status(404).json({
-        message: "No appointment found for the given name and phone number.",
-      });
-    } else {
-      const appointment_date = moment
-        .tz(results[0].appointment_date, "Asia/Shanghai")
-        .format("YYYY-MM-DD HH:mm");
-      res.json({ appointment_date });
-    }
-  });
-});
-
-router.post("/cancel", (req, res) => {
-  const { name, phone } = req.body;
-
-  console.log("Request body:", req.body);
-
-  // Input validation
-  if (!name || !phone) {
-    res.status(400).json({ message: "姓名和电话不能为空。" });
-    return;
-  }
-  // TODO: Add additional validation for phone number format, etc.
-
-  cancelAppointment(name, phone, (error, results) => {
-    if (error) {
-      console.error(error);
-      res.status(500).json({ message: "无法取消预约，请稍后重试。" });
-    } else if (results.affectedRows === 0) {
-      res.status(404).json({ message: "未找到匹配的预约记录。" });
-    } else {
-      console.log(`Deleted ${results.affectedRows} rows.`);
-      res.status(200).json({ message: `预约取消成功` });
-    }
-  });
 });
 
 // Handle message push
