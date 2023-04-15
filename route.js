4383fe7bb64280a55148d03017b4f6b149bb661c
Ranonx <108208227+Ranonx@users.noreply.github.com>
2023-04-15 16:48:54 +0800

    subscription event: msg + article

--- route.js
+++ route.js
@@ -16,21 +16,41 @@ router.post("/", async (req, res) => {
     if (Event === "subscribe") {
       const appid = req.headers["x-wx-from-appid"] || "";
       try {
-        const result = await sendmess(appid, {
+        // Send the original message
+        const result1 = await sendmess(appid, {
           touser: FromUserName,
           msgtype: "text",
           text: {
             content: `感谢关注伊甸数智医疗技术！我们专注于数字化医疗产品的研发和生产，致力于为全球患者提供高品质的医疗服务。
-    
-  我们提供以下服务：
-  1. 在线预约服务，方便您的就诊体验。点击<a href="https://ud8dwa5smb.jiandaoyun.com/f/643915f4aa01e10008bb341b">「预约到店」</a>。
-
-  2. 预约查询服务，快速查询您的预约。点击<a href="https://ud8dwa5smb.jiandaoyun.com/q/643915f4aa01e10008bb341b">「点我查询」</a>。
   
-  期待与您一同探索数字化医疗技术的未来。`,
+    我们提供以下服务：
+    1. 在线预约服务，方便您的就诊体验。点击<a href="https://ud8dwa5smb.jiandaoyun.com/f/643915f4aa01e10008bb341b">「预约到店」</a>。
+  
+    2. 预约查询服务，快速查询您的预约。点击<a href="https://ud8dwa5smb.jiandaoyun.com/q/643915f4aa01e10008bb341b">「点我查询」</a>。
+    
+    期待与您一同探索数字化医疗技术的未来。`,
           },
         });
-        console.log("发送消息成功", result);
+        console.log("发送消息成功", result1);
+
+        // Send the new article
+        const result2 = await sendmess(appid, {
+          touser: FromUserName,
+          msgtype: "news",
+          news: {
+            articles: [
+              {
+                title: "迈向康复的智能化未来",
+                description: "了解更多关于伊甸数智医疗技术的信息",
+                url: "http://www.eddiorthopros.com/",
+                picurl:
+                  "http://42.193.219.49/wp-content/themes/eddi-ortho-pros-lab/assets/images/tpu-_080223.jpeg",
+              },
+            ],
+          },
+        });
+        console.log("发送消息成功", result2);
+
         res.send("success");
       } catch (error) {
         console.log("发送消息失败", error);
